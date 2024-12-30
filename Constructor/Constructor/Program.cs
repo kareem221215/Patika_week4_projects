f290a2f43d8ce74dac24f3325585ad0c66f35aa2
kareem <120470069+kareem221215@users.noreply.github.com>
2024-12-30 22:35:46 +0300

    Add files via upload

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            baby baby1 = new baby();
+            Console.WriteLine($"baby 1 - name {baby1.Firstname} - surname {baby1.Lastname} - DOB {baby1.Dateofbirth.ToShortDateString()}");
+
+            baby baby2 = new baby("ahmet" , "yalmaz");
+            Console.WriteLine($"baby 2 - name {baby2.Firstname} - surname {baby2.Lastname} - DOB {baby2.Dateofbirth.ToShortDateString()}");
+
+            Console.ReadLine();
+        }
+        public class baby
+        {
+            public string Firstname { get; set; }
+            public string Lastname {  get; set; }
+            public DateTime Dateofbirth { get; set; }
+            public baby()
+            {
+                Firstname = string.Empty;
+                Lastname = string.Empty;
+                Dateofbirth = DateTime.Now;
+            }
+            public baby(string firstname, string lastname)
+            {
+                Firstname = firstname;
+                Lastname = lastname;
+                Dateofbirth = DateTime.Now;
+                Console.WriteLine("ingaaaa");
+            }
+        }
+    }
+}
